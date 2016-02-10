87688cbefbbb36c3ccfd948dd26c2c625853e185
KristenGarnier <garnier.kristen@icloud.com>
2016-02-10 12:04:31 +0100

    Adding a working navigation

---
+++ js/home.js
@@ -0,0 +1,139 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+    Component,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import Button from 'apsl-react-native-button';
+import {objectToArray, fetchUser} from './utils';
+import ExNavigator from '@exponent/react-native-navigator';
+import Routes from './routes';
+
+const dbUsers = 'https://clipin.firebaseio.com/users.json';
+const dbUsersRequest = 'https://clipin.firebaseio.com/users';
+const dbBase = 'https://clipin.firebaseio.com';
+
+class clipin extends Component {
+
+    constructor() {
+        super();
+
+        this._handlePress = this._handlePress.bind(this);
+        this._handleNav = this._handleNav.bind(this);
+
+        this.request = fetch(dbUsers)
+            .then(res => res.json());
+
+        this.state = {
+            users: []
+        }
+
+
+    }
+
+    componentDidMount() {
+        this.request
+            .then(res => {
+                this.setState({
+                    users: objectToArray(res)
+                });
+
+            })
+            .catch(error => console.error(error));
+    }
+
+    render() {
+
+        const users = this.state.users.map((item, i) => {
+            return <Text key={i} style={styles.instructions}>
+                {item.infos.nom} {item.infos.prenom}
+            </Text>;
+        });
+
+        return (
+            <View style={styles.container}>
+                {users}
+                <Button style={{backgroundColor: 'transparent'}} textStyle={{fontSize: 18}} onPress={this._handlePress}>
+                    Add infos in db
+                </Button>
+                <Button style={{backgroundColor: 'transparent'}} textStyle={{fontSize: 18}} onPress={this._handleNav}>
+                    Navigator
+                </Button>
+            </View>
+        );
+    }
+
+    _handleNav(){
+        const route = Routes.getPuckRoute();
+        this.props.navigator.push(route);
+    }
+
+    _handlePress() {
+        fetch(dbUsers, {
+            method: 'POST',
+            body: JSON.stringify({
+                UUID: 'ZERTH4REF',
+                totalRencontres: 2,
+                infos: {
+                    nom: 'Montelimard',
+                    prenom: 'Benjamin',
+                    socials: {
+                        facebook: 'https://facebook.com'
+                    }
+                },
+                relations: [
+                    {
+                        UUID: '12343FZE33',
+                        compatibilite: 60,
+                        date: '12-01-16',
+                        nom: 'Vialaneix',
+                        prenom: 'Juliette'
+                    }
+                ]
+            })
+        })
+            .then(res => res.json())
+            .then(res => {
+                fetchUser(res.name, dbUsersRequest)
+                    .then(res => {
+                        this.setState({
+                            users: [
+                                ...this.state.users,
+                                res
+                            ]
+                        });
+                    })
+                    .catch(err => console.error(err));
+            })
+            .catch(err => console.error(res));
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+export default clipin;
+
+
---
+++ js/routes.js
@@ -0,0 +1,50 @@
+import home from './home'
+import puck from './puck'
+export default routes = {
+    getHomeRoute() {
+        return {
+            // Return a React component class for the scene. It receives a prop
+            // called `navigator` that you can use to push on more routes.
+            getSceneClass() {
+                return home;
+            },
+
+            // When this scene receives focus, you can run some code. We're just
+            // proxying the `didfocus` event that Navigator emits, so refer to
+            // Navigator's source code for the semantics.
+            onDidFocus(event) {
+                console.log('Home Scene received focus.');
+            },
+
+            // Return a string to display in the title section of the navigation bar.
+            // This route's title is displayed next to the back button when you push
+            // a new route on top of this one.
+            getTitle() {
+                return 'Home';
+            }
+        };
+    },
+    getPuckRoute() {
+        return {
+            // Return a React component class for the scene. It receives a prop
+            // called `navigator` that you can use to push on more routes.
+            getSceneClass() {
+                return puck;
+            },
+
+            // When this scene receives focus, you can run some code. We're just
+            // proxying the `didfocus` event that Navigator emits, so refer to
+            // Navigator's source code for the semantics.
+            onDidFocus(event) {
+                console.log('Home Scene received focus.');
+            },
+
+            // Return a string to display in the title section of the navigation bar.
+            // This route's title is displayed next to the back button when you push
+            // a new route on top of this one.
+            getTitle() {
+                return 'Puck';
+            }
+        };
+    }
+};
