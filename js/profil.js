8dd0345b2e06ba53a05e9ce86fe6e6c314d5cacf
KristenGarnier <garnier.kristen@icloud.com>
2016-02-12 12:00:30 +0100

    Put row data outside the profile view

--- js/profil.js
+++ js/profil.js
@@ -20,75 +20,7 @@ import Routes from './routes';
 import Divider from './components/tableDivider';
 import Row from './components/tableRow';
 import Table from './components/tableParam';
-
-const advanced = [
-    {
-        name : 'Hobbies',
-        content : 'future toggle'
-    },
-    {
-        name : 'Formation',
-        content : 'future toggle'
-    },
-    {
-        name : 'Expérience',
-        content : 'future toggle'
-    },
-    {
-        name : 'Compétences',
-        content : 'future toggle'
-    },
-    {
-        name : 'Préférences',
-        content : 'future toggle'
-    },
-    {
-        name : 'Permis',
-        content : 'future toggle'
-    },
-    {
-        name : 'Salaire minimum',
-        content : 'future toggle'
-    }
-];
-
-const general = [
-    {
-        name : 'Numéro de téléphone',
-        content : 'future toggle'
-    },
-    {
-        name : 'Nom & prénom',
-        content : 'future toggle'
-    },
-    {
-        name : 'Âge',
-        content : 'future toggle'
-    },
-    {
-        name : 'Adresse',
-        content : 'future toggle'
-    },
-    {
-        name : 'Mail',
-        content : 'future toggle'
-    },
-    {
-        name : 'Profession',
-        content : 'future toggle'
-    }
-];
-
-const criteria = [
-    {
-        name : 'Je suis',
-        content : 'Ouvert(e) aux opportunités'
-    },
-    {
-        name : 'Je recherche',
-        content : 'Un emploi'
-    }
-];
+import {criteria, advanced, general} from './tableData';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
---
+++ js/tableData.js
@@ -0,0 +1,68 @@
+export const advanced = [
+    {
+        name : 'Hobbies',
+        content : 'future toggle'
+    },
+    {
+        name : 'Formation',
+        content : 'future toggle'
+    },
+    {
+        name : 'Expérience',
+        content : 'future toggle'
+    },
+    {
+        name : 'Compétences',
+        content : 'future toggle'
+    },
+    {
+        name : 'Préférences',
+        content : 'future toggle'
+    },
+    {
+        name : 'Permis',
+        content : 'future toggle'
+    },
+    {
+        name : 'Salaire minimum',
+        content : 'future toggle'
+    }
+];
+
+ export const general = [
+    {
+        name : 'Numéro de téléphone',
+        content : 'future toggle'
+    },
+    {
+        name : 'Nom & prénom',
+        content : 'future toggle'
+    },
+    {
+        name : 'Âge',
+        content : 'future toggle'
+    },
+    {
+        name : 'Adresse',
+        content : 'future toggle'
+    },
+    {
+        name : 'Mail',
+        content : 'future toggle'
+    },
+    {
+        name : 'Profession',
+        content : 'future toggle'
+    }
+];
+
+export const criteria = [
+    {
+        name : 'Je suis',
+        content : 'Ouvert(e) aux opportunités'
+    },
+    {
+        name : 'Je recherche',
+        content : 'Un emploi'
+    }
+];
